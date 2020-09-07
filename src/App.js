dfa804d310318849eb7137b25c5d012df60fab04
Divyanshi Agrawal <divsagr3@gmail.com>
2020-09-07 16:06:12 +0530

    player

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import './App.css';
-import Login from './Login';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+//Global instance of spotify, responsible for every communication betn our react app and actual spotify
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
+  const [token, setToken] = useState(null);
 
+  //Run code based on a condition
+  //Runs once if empty brackets given, if brackets have something
+  //then runs everytime that variable changes
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
 
-      <Login />
-    </div>
-  );
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      //giving access token to the spotify api for all communication betn spotify services and reactApp
+      spotify.setAccessToken(_token);
+
+      //getMe --> gets the user's account and then returning that user
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
+    console.log("I have a token", _token);
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
