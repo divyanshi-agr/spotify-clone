202ddce54bd293074f619cfca7503ed6acc3fc19
Divyanshi Agrawal <divsagr3@gmail.com>
2020-09-17 21:12:07 +0530

    playlist

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
@@ -10,9 +13,17 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
       />
 
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+      <br />
+      <stong className="sidebar-title">PLAYLISTS</stong>
+      <hr />
+
+      <SidebarOption title="Hip Hop" />
+      <SidebarOption title="Country" />
+      <SidebarOption title="Rock" />
     </div>
   );
 }
